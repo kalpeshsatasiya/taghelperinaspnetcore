842198a82b00a1911f261768d8ff077afff3dcd0
Kalpesh Satasiya <satasiya.kalpesh2006@gmail.com>
2019-07-27 15:37:33 +0530

    This is a new commit for what I originally planned to be amended

---
+++ TagHelpers/SecureContentTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace taghelperdemo.TagHelpers
+{
+    [HtmlTargetElement("secure-content")]
+    public class SecureContentTagHelper : TagHelper
+    {
+        public List<string> PageNames { get; set; }
+        [HtmlAttributeName("asp-page-name")]
+        public string PageName { get; set; }
+
+        public SecureContentTagHelper()
+        {
+            PageNames = new List<string>
+            {
+                "Home",
+                "About",
+                "Contact",
+                "Help"
+            };
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "";
+
+            if (PageNames.Any(x => x == PageName))
+            {
+                return;
+            }
+
+            output.SuppressOutput();
+        }
+
+    }
+}
---
+++ TagHelpers/SecureLinkTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace taghelperdemo.TagHelpers
+{
+    [HtmlTargetElement("secure-link")]
+    public class SecureLinkTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-page-name")]
+        public string PageName { get; set; }
+
+        public List<string> PageNames { get; set; }
+
+        public SecureLinkTagHelper()
+        {
+            PageNames = new List<string>
+            {
+                "Home",
+                "About",
+                "Contact"
+            };
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "";
+
+            if(PageNames.Any(x=>x == PageName))
+            {
+                return;
+            }
+            output.SuppressOutput();
+        }
+    }
+}
